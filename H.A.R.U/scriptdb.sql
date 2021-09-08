26221099377d172f10265ca4e8c410370452da49
Nghia-Hieu <63239928+Nghia-Hieu@users.noreply.github.com>
2021-09-08 16:43:25 +0700

    Update scriptdb.sql

--- H.A.R.U/scriptdb.sql
+++ H.A.R.U/scriptdb.sql
@@ -133,6 +133,7 @@ INSERT INTO comments VALUES
 (7, 7, 1, CAST(N'2021-08-30 01:39:30' AS DateTime), 3, N'Sản phẩm khá nhỏ @@', 1, 1),
 (8, 6, 2, CAST(N'2021-08-30 01:39:30' AS DateTime), 5, N'Thật sự dễ thương ^^ rất hài lòng', 1, NULL),
 (9, 8, 11, CAST(N'2021-08-30 01:39:30' AS DateTime), 5, N'Màu rất nổi bật, có thể phối với nhiều trang phục', 1, NULL),
-(10, 4, 11, CAST(N'2021-08-30 01:39:30' AS DateTime), 5, N'Thật sao? mình sẽ mua thử xem sao', 1, 9);
+(10, 4, 11, CAST(N'2021-08-30 01:39:30' AS DateTime), 5, N'Thật sao? mình sẽ mua thử xem sao', 1, 9),
+(11, 15, 14, CAST(N'2021-08-30 02:35:13' AS DateTime), 3, N'Chất lượng tệ, không giống như hình ', 1, NULL);
 
 
