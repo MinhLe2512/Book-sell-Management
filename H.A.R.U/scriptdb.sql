9f2e7deebe5b5a61ab8e21fa13a40b4f94ce3332
Mac Hung <machung2001@gmail.com>
2021-09-10 13:34:03 +0700

    Update scriptdb.sql

--- H.A.R.U/scriptdb.sql
+++ H.A.R.U/scriptdb.sql
@@ -148,7 +148,8 @@ INSERT INTO comments VALUES
 (11, 14, 13, CAST(N'2021-09-01 01:00:30' AS DateTime), 1, N'Sản phẩm không đúng như trên hình', 1, NULL),
 (12, 13, 12, CAST(N'2021-09-01 01:00:30' AS DateTime), 5, N'Đẹp', 1, NULL),
 (13, 12, 11, CAST(N'2021-09-01 23:00:30' AS DateTime), 4, N'Vừa vặn mà màu hơi phai -1 sao', 1, NULL),
-(14, 11, 10, CAST(N'2021-09-01 23:00:30' AS DateTime), 5, N'Oke nice, nhớ sao kê', 1, NULL);
+(14, 11, 10, CAST(N'2021-09-01 23:00:30' AS DateTime), 5, N'Oke nice, nhớ sao kê', 1, NULL),
+(15, 15, 19, CAST(N'2021-09-07 07:00:30' AS DateTime), 5, N'Cái nịt thì vừa còn cái vòng thì oke', 1, NULL);
 
 /* Le Minh chinh sua o day */
 INSERT INTO userLikeComment VALUES
